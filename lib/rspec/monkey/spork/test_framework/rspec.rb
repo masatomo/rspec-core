938f87ce57e216a262485a5a05baa675d8d16309
Masatomo Nakano <masatomon@reallyenglish.com>
2010-07-10 23:36:16 +0100

    RSpec::Core::CommandLine initializer requires Rspec::Core::ConfigurationOptions not Array

--- lib/rspec/monkey/spork/test_framework/rspec.rb
+++ lib/rspec/monkey/spork/test_framework/rspec.rb
@@ -1,7 +1,9 @@
 if defined?(Spork::TestFramework::RSpec)
   class Spork::TestFramework::RSpec < Spork::TestFramework
     def run_tests(argv, err, out)
-      ::RSpec::Core::CommandLine.new(argv).run(err, out)
+      options = ::Rspec::Core::ConfigurationOptions.new(argv)
+      options.parse_options
+      ::RSpec::Core::CommandLine.new(options).run(err, out)
     end
   end
 end
